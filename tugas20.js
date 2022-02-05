9192983567557eb5a3a813ef52977a66cee70119
sayra17 <agussayra03@gmail.com>
2022-02-05 16:54:56 +0700

    Initial commit

---
+++ tugas20.js
@@ -0,0 +1,6 @@
+function temukan(x) {
+    var kata = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+    console.log(kata.match(x))
+}
+temukan("are")
+temukan("words")
